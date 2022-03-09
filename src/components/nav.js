0f4352119c5e1d2c29b63d1a0ecfa0adec4dc2f9
budds300 <budds300@gmail.com>
2022-03-09 21:06:01 +0300

    design

--- src/components/nav.js
+++ src/components/nav.js
@@ -7,26 +7,34 @@ function Navi (props){
     <div>
 
     <Container>
-    
-    <ul class="nav justify-content-end pt-5">
+    <div className='d-flex' >
+
+   
+    <ul class="nav justify-content-end pt-5 ">
+  <li class="nav-item">
+    <a class="nav-link text-black pe-5" href="#">logo <img alt='' src= {props.image} className="" height={30} width={30} /></a>
+  </li>
+  </ul>
+    <ul class="nav justify-content-center pt-5 ">
+ 
   <li class="nav-item ">
     <a class="nav-link active text-black px-5" aria-current="page" href="#">Courses</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link text-black px-5" href="#">Studies</a>
+    <a class="nav-link text-black px-3" href="#">Studies</a>
   </li>
   <li class="nav-item">
-    <a class="nav-link text-black px-5" href="#">Puzzles</a>
+    <a class="nav-link text-black px-3" href="#">Puzzles</a>
   </li>
-  <form class="d-flex px-5">
+  <form class="d-flex px-3">
       <input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-      <button class="btn btn-outline-dark" type="submit">Search</button>
+      <button class="btn btn-light" type="submit">Search</button>
     </form>
   <li class="nav-item">
-    <a class="nav-link text-black px-5" href="#">Mwangi <img alt='' src= {props.image} className="" height={30} width={30} /></a>
+    <a class="nav-link text-black px-3" href="#">Mwangi <img alt='' src= {props.image} className="" height={30} width={30} /></a>
   </li>
-  
 </ul>
+    </div>
     
     </Container>
     
